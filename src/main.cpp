96bb91e7bce73491a5fa6901f5a79c10ae5c90b8
Arcayth <h.chanouni4444@uca.ac.ma>
2025-01-17 00:22:45 +0100

    add echo command

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,34 @@
 
 using namespace std;
 
+enum CMD {
+    EXIT,
+    ECHO,
+    UNKNOWN,
+};
+
+CMD command_parser(string input){
+    string cmd = input.substr(0, input.find(" "));
+    if (cmd == "exit") return EXIT;
+    if (cmd == "echo") return ECHO;
+
+    return UNKNOWN;
+}
+
+void handle_exit() {
+    exit(0);
+}
+
+void handle_echo(string input) {
+    string args = input.substr(input.find("echo "));
+    cout << args << endl;
+}
+
+void handle_unknown(string input) {
+    string cmd = input.substr(0, input.find(" "));
+    cout << cmd << ": command not found\n";
+}
+
 int main() {
     // Flush after every std::cout / std:cerr
     cout << unitbuf;
@@ -12,13 +40,14 @@ int main() {
 
     while (true) {
         cout << "$ ";
-
         getline(cin, input);
-        string cmd = input.substr(0, input.find(" "));
-        if (cmd == "exit") {
-            exit(0);
+        CMD cmd = command_parser(input);
+
+        switch (cmd) {
+            case EXIT: exit(0);
+            case ECHO: exit(0);
+            case UNKNOWN: exit(0);
         }
 
-        cout << input << ": command not found\n";
     }
 }
