5b5be17d546b9c21ea6d67164cae3a5727684076
Arcayth <h.chanouni4444@uca.ac.ma>
2025-01-17 00:36:51 +0100

    add echo command

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,14 @@ using namespace std;
 enum CMD {
     EXIT,
     ECHO,
+    TYPE,
     UNKNOWN,
 };
 
-CMD command_parser(string input){
-    string cmd = input.substr(0, input.find(" "));
+CMD command_parser(string cmd){
     if (cmd == "exit") return EXIT;
     if (cmd == "echo") return ECHO;
+    if (cmd == "type") return TYPE;
 
     return UNKNOWN;
 }
@@ -25,6 +26,23 @@ void handle_echo(string input) {
     // 5 is the lenght of "echo "
     string args = input.substr(5);
     cout << args << endl;
+}
+
+void handle_type(string input) {
+    // 5 is the lenght of "type "
+    string args = input.substr(5);
+    string cmd = args.substr(0, input.find(" "));
+
+    switch (command_parser(cmd)) {
+        case UNKNOWN:
+            cout << cmd << ": not found:" << endl;
+            break;
+        default:
+            cout << cmd << " is a shell builtin" << endl;
+    }
+
+
+    cout << cmd << ": command not found\n";
 }
 
 void handle_unknown(string input) {
@@ -42,14 +60,18 @@ int main() {
     while (true) {
         cout << "$ ";
         getline(cin, input);
-        CMD cmd = command_parser(input);
+        string cmd = input.substr(0, input.find(" "));
 
-        switch (cmd) {
+        switch (command_parser(cmd)) {
             case EXIT:
                 handle_exit();
 
             case ECHO:
                 handle_echo(input);
+                break;
+
+            case TYPE:
+                handle_type(input);
                 break;
 
             case UNKNOWN:
